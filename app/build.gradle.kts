cf80488e26f521d466d4cf6fac4279a8213c1729
Gaethan Ramos <gramosmed@comunidad.unam.mx>
2025-04-06 17:38:35 -0600

    Avances iniciales - División de pantalla

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,11 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation("com.google.android.material:material:1.8.0")
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
