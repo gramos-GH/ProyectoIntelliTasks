abc555b6f0961fc99240fdbc61b589ed0e6444dc
Gaethan Ramos <gramosmed@comunidad.unam.mx>
2025-04-07 18:23:50 -0600

    Implementación inicial del nav_graph.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation ("com.airbnb.android:lottie:5.0.3")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
